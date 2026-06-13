8534b3243be5edbed91bc0601be4bcc12db50fe1
epixplaysyt <joshuamulchrone@gmail.com>
2026-06-13 19:51:00 +0100

    Update __init__.py

--- ticketmail/__init__.py
+++ ticketmail/__init__.py
@@ -1,6 +1,10 @@
-from .ticketmail import Modmail
+from .modmail import Modmail
 
 async def setup(bot):
     cog = Modmail(bot)
     await bot.add_cog(cog)
+    try:
+        bot.tree.remove_command(cog.ticket_group.name)
+    except Exception:
+        pass
     bot.tree.add_command(cog.ticket_group)
